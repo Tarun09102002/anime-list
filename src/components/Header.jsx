9d80bd2ae7745f94daa92be57a4bad66358c4721
Tarun <tarun.agrawal@spit.ac.in>
2022-07-06 00:01:13 +0530

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { SearchIcon } from '@heroicons/react/outline'
+import { SearchIcon, MenuIcon } from '@heroicons/react/outline'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 
 function Header({ purple }) {
     const [search, setSearch] = useState('')
+    const [isHidden, setIsHidden] = useState(true)
     const navigate = useNavigate()
+
+    const handleClick = (state) => {
+        // console.log(state)
+        setIsHidden(!isHidden)
+    }
+
+    const refs = [
+        { desc: 'Top Anime', link: '/' },
+        { desc: 'Releasing Today', link: '/anime/releasingtoday' },
+        { desc: 'Random Anime', link: '/anime/random' },
+        { desc: 'Top Manga', link: '/' }]
 
     const handleSearch = (e) => {
         // e.preventDefault();
@@ -14,19 +26,19 @@ function Header({ purple }) {
     }
 
     return (
-        <div className={`flex flex-row py-5 h-20 justify-evenly border-b ${!purple ? 'border-purple-900' : 'border-white'} `}>
-            <div className={`flex items-center flex-row w-1/2 justify-between ${!purple ? 'text-purple-900' : 'text-white'}  `}>
-                <a href="/">Top Anime</a>
-                <a href="/anime/releasingtoday">Releasing Today</a>
-                <a href="/anime/random">Random Anime</a>
-                <a href="/">Top Manga</a>
+        <div className={`flex md:flex-row flex-col items-center py-5 md:h-20 md:justify-evenly border-b ${!purple ? 'border-purple-900' : 'border-white'} `}>
+            <MenuIcon className={`md:hidden w-[30px] ${!purple ? 'text-purple-900' : 'text-white'} `} onClick={handleClick}></MenuIcon>
+            <div className={`flex duration-1000 ease-linear ${isHidden ? 'hidden' : ''} md:flex items-center flex-col md:w-1/2 md:flex-row md:justify-between md:items-center ${!purple ? 'text-purple-900' : 'text-white'}  `}>
+                {refs.map((ref, index) => {
+                    return <a key={index} href={ref.link} className='mt-2'>{ref.desc}</a>
+                })}
             </div>
-            <div className='flex flex-row items-center w-1/4'>
-                <SearchIcon className={`h-2/3 w-1/6 rounded-lg ${!purple ? 'text-purple-900' : 'text-white'}   py-1`} strokeWidth={2}></SearchIcon>
-                <form className='w-5/6' onSubmit={handleSearch}>
+            <div className={`flex ${isHidden ? 'hidden' : ''} mt-2 md:flex flex-row items-center justify-center md:w-1/4`}>
+                <SearchIcon className={`md:w-[30px] md:h-[30px] hidden sm:flex ${!purple ? 'text-purple-900' : 'text-white'}`} strokeWidth={2}></SearchIcon>
+                <form className='' onSubmit={handleSearch}>
                     <input placeholder='Search Anime...'
                         onChange={e => setSearch(e.target.value)}
-                        className={`focus:outline-none ${!purple ? 'bg-purple-900 ' : 'bg-white'} rounded-lg ${!purple ? 'text-white' : 'text-purple-900'} px-4 py-1`}
+                        className={` focus:outline-none ${!purple ? 'bg-purple-900 text-white' : 'bg-white text-purple-900'} rounded-lg px-4 py-1`}
                     ></input>
                 </form>
             </div>
