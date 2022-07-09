7e9fd87183d6d5269d7c541cb939662656b50bd9
Tarun <tarun.agrawal@spit.ac.in>
2022-07-09 13:24:10 +0530

    anime card updated

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -11,7 +11,7 @@ function AnimeCard({ anime }) {
     return (
         <article className='flex flex-col items-center'>
             <figure className='hover:cursor-pointer' onClick={loadDetails}>
-                <img src={anime.images?.jpg?.image_url} className='rounded-lg shadow-2xl object-cover h-[200px]' />
+                <img src={anime?.images?.jpg?.image_url} className='rounded-lg shadow-2xl object-cover h-[200px]' />
             </figure>
             <h3 className='pt-5 text-center text-purple-900 hover:cursor-pointer' onClick={loadDetails}>{anime.title}</h3>
         </article>
